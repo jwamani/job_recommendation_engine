32e8b0cd995da51dcb34551b960a60606e569f18
p2k3 <pnamugerwa2@gmail.com>
2026-07-20 16:13:48 +0300

    Pointing Djnago to project level  static and templaes folders

--- job_recommendation_engine/settings.py
+++ job_recommendation_engine/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'job_recommendation_engine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -70,6 +70,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'job_recommendation_engine.wsgi.application'
 
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
