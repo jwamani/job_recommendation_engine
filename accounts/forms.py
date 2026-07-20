95e0758448d53a25c73492b8711dd69646141f8f
p2k3 <pnamugerwa2@gmail.com>
2026-07-20 20:13:17 +0300

    Added form handling logic for  register and login in accounts

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name in ('username', 'password1', 'password2'):
+            self.fields[field_name].widget.attrs.update({'class': 'form-control'})
