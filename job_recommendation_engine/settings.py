601a5b03e1aebce3d3e8c929f41665321278240c
p2k3 <pnamugerwa2@gmail.com>
2026-07-20 15:58:27 +0300

    added our apps to installed apps in settings.py

--- job_recommendation_engine/settings.py
+++ job_recommendation_engine/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # our apps
+    'accounts',
+    'jobs',
+    'interactions',
+    'matching',
 ]
 
 MIDDLEWARE = [
