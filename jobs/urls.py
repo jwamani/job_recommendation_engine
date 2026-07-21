d0a347ed56bedd71369ee3798e9200f6df0fb378
Namutebi Sulaina Luwambya <sulainanamutebi@gmail.com>
2026-07-21 10:09:14 +0300

    added path to job list and job details views.

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 
+from .views import JobDetailView, JobListView
+
 app_name = "jobs"
 
-urlpatterns = []
+urlpatterns = [
+	path("", JobListView.as_view(), name="job_list"),
+	path("jobs/<int:pk>/", JobDetailView.as_view(), name="job_detail"),
+]
