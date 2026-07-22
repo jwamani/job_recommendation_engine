a4e9bcf5787aa6ca458dcaa4e0dd4fcf70d4336a
Namutebi Sulaina Luwambya <sulainanamutebi@gmail.com>
2026-07-22 14:25:41 +0300

    ...

--- interactions/urls.py
+++ interactions/urls.py
@@ -7,4 +7,5 @@ app_name = "interactions"
 urlpatterns = [
     path("log/", views.log_interaction, name="log"),
     path("saved/", views.saved_jobs, name="saved_jobs"),
+    path("applied/<int:pk>/", views.application_submitted, name="application_submitted"),
 ]
